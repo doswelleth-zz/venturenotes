54001951baff00251a576845a75e4730bb33c3fd
David O. Doswell <daviddoswell@lambda-school-loaner-11s-macbook-pro.local>
2018-10-31 14:36:31 -0700

    added glossary c-m

--- venturenotes/Model/Strings.swift
+++ venturenotes/Model/Strings.swift
@@ -14,19 +14,19 @@ struct SectionStrings {
 
 struct ArrayStrings {
     let array = [
-        ["Analyst", "Angel Investor", "ARR", "Associate", "AUM",],
-        ["Board","Burn (Burn Rate)", "Buyback"],
-        ["C"],
-        ["D"],
-        ["E"],
-        ["F"],
-        ["G"],
-        ["H"],
-        ["I"],
+        ["Analyst", "Angel Investor", "Accounting Rate of Return (ARR)", "Associate", "Assets Under Management (AUM)"],
+        ["Board", "Bubble", "Burn (Burn Rate)", "Buyback"],
+        ["Carry", "Common Stock", "Convertible Note"],
+        ["DAU", "Deal Flow", "Debt (Venture Debt)", "Discount Rate", "Down Round"],
+        ["Equity"],
+        ["Follow-On"],
+        ["General Counsel (GC)", "General Partner (GP)", "Growth Rate"],
+        ["High Growth"],
+        ["Independent Director", "Internal Rate of Return (IRR)", "Initial Coin Offering (ICO)", "Initial Public Offering (IPO)"],
         ["J"],
         ["K"],
-        ["L"],
-        ["M"],
+        ["Lead Investor", "Leveraged Buyout", "Limited Partner (LP)", "Liquidation Preference"],
+        ["Merger and Acquisitions (M&A)", "MAU", "Multiple"],
         ["N"],
         ["O"],
         ["P"],
@@ -45,31 +45,57 @@ struct ArrayStrings {
 
 struct DetailArrayStrings {
     let detailArray = [
-        ["Associate researching market sizes and opportunities", "Early stage imvestor ($50,000 - $2,000,000)", "Annual Recurring Revenue", "Junior member at a venture capital firm", "Assets Under Management"],
-        ["Elected group of individuals representing shareholders", "The rate at which a company is spending money", "When a company buys its own outstanding shares"],
+        // A
+        ["Associate researching market sizes and opportunities", "Early stage imvestor ($50,000 - $2,000,000)", "Measured amount of profit expected on an investment", "Junior member at a venture capital firm", "Total market value of managed assets by a firmventure c"],
+        // B
+        ["Elected group of individuals representing shareholders", "Artificial and irrational increase in asset prices", "The rate at which a company is spending money", "When a company buys its own outstanding shares"],
+        // C
+        ["Profit for Partners when portfolio companies are sold", "Corporate equity ownership with voting rights", "Short-term debt that converts into equity in the future"],
+        // D
+        ["Daily Active Users", "Rate of proposals received by venture investors", "Non-equity growth financing for capital expenses", "Rate used to compute present value", "Stock purchase at a lower valuation than former round"],
+        // E
+        ["A stock or security representing an ownership interest"],
+        // F
+        ["Subsequent investment made by a current investor"],
+        // G
+        ["Executive managing a firm's legal and regulatory risk", "Owner of a partnership who has unlimited liability", "Compounded rate of growth of a company's revenues"],
+        // H
+        ["Asset that increases quickly in value"],
+        // I
+        ["Board member brought in from outside the company", "Rule used to evaluate projects in capital budgeting", "Cryptographic coin offering by a decentralized team", "First time share offering on securities exchanges"],
+        // J
         [""],
+        // K
         [""],
+        // L
+        ["Investor investing the most capital in a round", "Company purchase with equity and borrowed money", "A wealthy individual investing in a venture capital firm", "Contract to specify which investors get paid first"],
+        // M
+        ["Consolidation of companies or assets in business", "Monthly Active Users", "How much investors pay per dollar of earnings"],
+        // N
         [""],
+        // O
         [""],
+        // P
         [""],
+        // Q
         [""],
+        // R
         [""],
+        // S
         [""],
+        // T
         [""],
+        // U
         [""],
+        // V
         [""],
+        // W
         [""],
+        // X
         [""],
+        // Y
         [""],
-        [""],
-        [""],
-        [""],
-        [""],
-        [""],
-        [""],
-        [""],
-        [""],
-        [""],
+        // Z
         [""]
     ]
 }

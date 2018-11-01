f5d9b5dcd2b497fc45d3e8fc6d46d8fb3f168587
David O. Doswell <daviddoswell@lambda-school-loaner-11s-macbook-pro.local>
2018-10-31 19:32:17 -0700

    completed elevator vc

---
+++ venturenotes/Pitch Model/Prototype.swift
@@ -0,0 +1,9 @@
+//
+//  Prototype.swift
+//  venturenotes
+//
+//  Created by David Doswell on 10/31/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import Foundation
---
+++ venturenotes/Views/DealCell.swift
@@ -0,0 +1,26 @@
+//
+//  DealCell.swift
+//  venturenotes
+//
+//  Created by David Doswell on 10/31/18.
+//  Copyright © 2018 David Doswell. All rights reserved.
+//
+
+import UIKit
+
+class DealCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpViews() {
+        
+    }
+}
